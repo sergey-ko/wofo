7236c1d3f1f511152ca86f9a6d06b17dea9a2b33
Sergey Kovalev <44134504+sergey-ko@users.noreply.github.com>
2024-09-15 11:07:04 +0700

    adopting for github pages

--- next.config.js
+++ next.config.js
@@ -1,19 +1,32 @@
+// Deployment to Azure Static Website
+
+// module.exports = {
+//   trailingSlash: true,
+//   modularizeImports: {
+//     '@mui/material': {
+//       transform: '@mui/material/{{member}}',
+//     },
+//     '@mui/lab': {
+//       transform: '@mui/lab/{{member}}',
+//     },
+//   },
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       use: ['@svgr/webpack'],
+//     });
+//     return config;
+//   },
+//   output:"standalone"
+// };
+
+// Deployment to Github Pages
+
 module.exports = {
-  trailingSlash: true,
-  modularizeImports: {
-    '@mui/material': {
-      transform: '@mui/material/{{member}}',
-    },
-    '@mui/lab': {
-      transform: '@mui/lab/{{member}}',
-    },
+  output: 'export', // Use static export
+  basePath: '',     // Optional, if you're using a custom domain
+  images: {
+    unoptimized: true // Disable image optimization for GitHub Pages
   },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
-  output:"standalone"
 };
+
